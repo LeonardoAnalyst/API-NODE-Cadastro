9d89e29c693ddeaccf85ea53456c8b5022c40e1a
Leozera <leobhal13@gmail.com>
2019-09-18 21:07:00 -0300

    Inicializando Authenticação con Json Web Token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
@@ -21,5 +21,24 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(400).send({ error: 'User not found' });
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ error: 'Invalid password' })
+    }
+
+    user.password = undefined;
+
+    const token = jwt.sign()
+
+    res.send({ user });
+});
 
 module.exports = app => app.use('/auth', router);
