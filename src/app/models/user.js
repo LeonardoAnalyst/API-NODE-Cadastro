87319bd15b31fc41d5f8095a8cda6799b2c3a04f
Leonardo Ramos Viana <31835188+LeozeraDeveloper@users.noreply.github.com>
2019-09-22 11:23:35 -0300

    Finalizando CRUD

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        require: true,
     },
     email: {
         type: String,
